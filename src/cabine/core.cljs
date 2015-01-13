a69bcab41a934b0a83187e8d06212892bdbd3685
raphaelboukara <raphael.boukara@gmail.com>
2015-01-14 00:08:17 +0200

    design cabine

--- src/cabine/core.cljs
+++ src/cabine/core.cljs
@@ -7,12 +7,11 @@
 
 (def noise-node (atom nil))
 
-(def cabine-state (atom {:title "Cabine"
-                         :C {:playing false :name :C}
-                         :L {:playing false :name :L}
-                         :SL {:playing false :name :SL}
-                         :R {:playing false :name :R}
-                         :SR {:playing false :name :SR}}))
+(def cabine-state (atom {:C {:playing false :name :C :img "center_speaker.png"}
+                         :L {:playing false :name :L :img "left_speaker.png"}
+                         :SL {:playing false :name :SL :img "left_speaker.png"}
+                         :R {:playing false :name :R :img "right_speaker.png"}
+                         :SR {:playing false :name :SR :img "right_speaker.png"}}))
 
 (defn speaker-playing-view? [playing]
   (when playing " playing"))
@@ -21,11 +20,13 @@
   (reify
     om/IRenderState
     (render-state [this {:keys [play]}]
-      (dom/div #js {:className (str "audyx-equipment"
-                                 (speaker-playing-view? (:playing speaker)))
+      (dom/div #js {:className (str "audyx-equipment "
+                                 "audyx-equipment-"
+                                 (name (:name @speaker))
+                                 (speaker-playing-view? (:playing @speaker)))
                     :onClick (fn [e] (put! play @speaker))}
-        (dom/img #js {:src "img/speaker.png"
-                      :width 55})))))
+        (dom/img #js {:src (str "img/" (:img @speaker))
+                      :width 52})))))
 
 (defn cabine-5-speakers-view [app owner]
   (reify
@@ -60,6 +61,7 @@
     om/IRenderState
     (render-state [this {:keys [play]}]
       (dom/div nil
+        (dom/div #js {:className "row"} nil)
         (dom/div #js {:className "row"}
           (dom/div #js {:className "col-md-4 col-md-offset-1"}
             (om/build speaker-view
@@ -68,7 +70,10 @@
           (dom/div #js {:className "col-md-4 col-md-offset-2"}
               (om/build speaker-view
                 (get app :R)
-                {:init-state {:play play}})))))))
+                {:init-state {:play play}})))
+        (dom/div #js {:className "row"}
+          (dom/div #js {:className "col-md-4 col-md-offset-2"} nil)
+          (dom/div #js {:className "col-md-4"} nil))))))
 
 (defn cabine-view [app owner]
   (reify
@@ -90,11 +95,10 @@
 
     om/IRenderState
     (render-state [this {:keys [play]}]
-      (dom/div nil
-        (dom/h1 nil (:title app))
-        (dom/div #js {:className "audyx-equipment-wrap"}
-          (if (> (audio/num-of-speakers) 2)
-            (om/build cabine-5-speakers-view app {:init-state {:play play}})
-            (om/build cabine-2-speakers-view app {:init-state {:play play}})))))))
+      (dom/div #js {:className "audyx-container"}
+          (dom/div #js {:className "audyx-equipment-wrap"}
+            (if (> (audio/num-of-speakers) 2)
+              (om/build cabine-5-speakers-view app {:init-state {:play play}})
+              (om/build cabine-2-speakers-view app {:init-state {:play play}})))))))
 
 (om/root cabine-view cabine-state {:target (. js/document (getElementById "cabine"))})
